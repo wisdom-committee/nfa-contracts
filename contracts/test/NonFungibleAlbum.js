9cf807283724744b7b67d6d2a3abd4900d2e16ff
César Daniel Pérez <cesdperez@gmail.com>
2022-01-22 21:21:17 +0100

    contracts: more function tests

--- contracts/test/NonFungibleAlbum.js
+++ contracts/test/NonFungibleAlbum.js
@@ -1,6 +1,7 @@
 const { expect } = require("chai");
+const { utils } = require("ethers");
 
-describe("NonFungibleAlbum contract", function () {
+describe("NonFungibleAlbum contract", async function () {
 
     const baseStickerURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
     const albumURI = "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/99999";
@@ -14,17 +15,36 @@ describe("NonFungibleAlbum contract", function () {
         contract = await contractFactory.deploy(name, symbol, baseStickerURI, albumURI, albumSize);
     });
 
-    describe("Deployment", function () {
+    describe("Deployment", async function () {
 
         it("Sets the right owner", async function () {
-            expect(await contract.owner()).to.equal(owner.address);
+            expect(await contract.owner()).to.be.equal(owner.address);
         });
     });
 
-    describe("Album functions", function () {
-        
+    describe("Functions", async function () {
+
         it("Gets album size", async function () {
-            expect(await contract.getAlbumSize()).to.equal(albumSize);
+            expect(await contract.getAlbumSize()).to.be.equal(albumSize);
+        });
+
+        it("Mints stickers", async function () {
+            txn = await contract.mintStickers(5, { value: utils.parseEther('0.005') });
+            expect(await txn.wait());
+        });
+
+        it("Gets stickers", async function () {
+            txn = await contract.mintStickers(5, { value: utils.parseEther('0.005') });
+            receipt = await txn.wait();
+
+            expect(await contract.getStickers(owner.address)).to.be.an('array').of.length(5);
+        });
+
+        it("Gets sticker URI", async function () {
+            txn = await contract.mintStickers(1, { value: utils.parseEther('0.005') });
+            receipt = await txn.wait();
+
+            expect(await contract.tokenURI(0)).to.contain(baseStickerURI);
         });
     });
 });
