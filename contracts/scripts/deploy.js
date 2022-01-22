01e309a74e892d4345bfd2dac156e7ba816ac632
César Daniel Pérez <cesdperez@gmail.com>
2022-01-22 23:09:17 +0100

    contracts: smoke tests cleanup

--- contracts/scripts/deploy.js
+++ contracts/scripts/deploy.js
@@ -22,46 +22,17 @@ async function main() {
   await contract.deployed();
   console.log("Contract deployed to:", contract.address);
 
-  await smokeTests(contract, owner);
+  await smokeTests(contract);
 }
 
-async function smokeTests(contract, owner) {
-  // Smoke tests: get album size, mint stickers, retrieve stickers, read stickers metadata
-  const albumSize = await contract.getAlbumSize();
-  console.log("Album size:", albumSize);
-
-  console.log("Minting 5 stickers");
-  let txn = await contract.mintStickers(5, { value: utils.parseEther('0.005') });
-  let receipt = await txn.wait();
-  printGas(receipt);
-
-  console.log("Owner has tokens: ", await contract.getStickers(owner.address));
+async function smokeTests(contract) {
+  console.log();
+  console.log("Smoke tests:");
+  
+  console.log("> Minting 5 stickers");
+  await contract.mintStickers(5, { value: utils.parseEther('0.005') });
 
   for (let i = 0; i < 5; i++) {
-    console.log("Sticker", i, "has URI:", await contract.tokenURI(i));
+    console.log("> Sticker", i, "has URI:", await contract.tokenURI(i));
   }
-
-  try {
-    console.log("Trying to mint - not full - album (should fail)");
-    let txn = await contract.mintAlbum({ value: utils.parseEther('0.01') });
-    receipt = await txn.wait()
-    printGas(receipt)
-  } catch (error) {
-    console.log("it failed!");
-  };
-
-  for (let i = 0; i < albumSize; i++) {
-    let txn = await contract.testMintSticker(i);
-    await txn;
-    console.log("Sticker", i, "minted");
-  }
-
-  console.log("Trying to mint - full - album (should work)");
-  txn = await contract.mintAlbum({ value: utils.parseEther('0.01') });
-  receipt = await txn.wait()
-  printGas(receipt)
-}
-
-function printGas(receipt) {
-  console.log('Ether spent on gas for txn:', utils.formatEther(receipt.cumulativeGasUsed.mul(receipt.effectiveGasPrice)))
 }
